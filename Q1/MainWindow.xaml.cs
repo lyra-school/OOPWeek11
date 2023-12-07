4459db3dd8217131d777d4a8db716358297bd4ad
lyra-school <s00233718@atu.ie>
2023-12-07 17:19:17 +0000

    Add project files.

---
+++ Q1/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Q1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Player> allPl = new List<Player>();
+        private List<Player> selectedPl = new List<Player>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                allPl.Add(new Player(Position.Goalkeeper));
+            }
+            for(int i = 0; i < 6; i++)
+            {
+                allPl.Add(new Player(Position.Defender));
+            }
+            for(int i = 0; i < 6; i++)
+            {
+                allPl.Add(new Player(Position.Midfielder));
+            }
+            for(int i = 0; i < 4; i++)
+            {
+                allPl.Add(new Player(Position.Forward));
+            }
+
+            allPl.Sort();
+
+            formations.ItemsSource = new string[] { "4-4-2", "4-3-3", "4-5-1" };
+            players.ItemsSource = allPl;
+            selectedPlayers.ItemsSource = selectedPl;
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (players.SelectedItem == null)
+            {
+                return;
+            }
+            if (selectedPl.Count >= 11)
+            {
+                errorLbl.Text = "Error: You have exceeded the maximum amount of space";
+                return;
+            }
+
+            if(!PositionValidator((Player)players.SelectedItem))
+            {
+                return;
+            }
+            errorLbl.Text = "";
+            selectedPl.Add((Player)players.SelectedItem);
+            allPl.Remove((Player)players.SelectedItem);
+
+            selectedPl.Sort();
+
+            players.ItemsSource = null;
+            players.ItemsSource = allPl;
+
+            selectedPlayers.ItemsSource = null;
+            selectedPlayers.ItemsSource = selectedPl;
+
+            
+            spacesLbl.Text = $"Spaces Available: {11 - selectedPl.Count}";
+        }
+
+        private void removeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(selectedPlayers.SelectedItem == null)
+            {
+                return;
+            }
+            errorLbl.Text = "";
+            allPl.Add((Player)selectedPlayers.SelectedItem);
+            selectedPl.Remove((Player)selectedPlayers.SelectedItem);
+            spacesLbl.Text = $"Spaces Available: {11 - selectedPl.Count}";
+
+            allPl.Sort();
+
+            players.ItemsSource = null;
+            players.ItemsSource = allPl;
+
+            selectedPlayers.ItemsSource = null;
+            selectedPlayers.ItemsSource = selectedPl;
+        }
+
+        private bool PositionValidator(Player player)
+        {
+            if(formations.SelectedItem == null)
+            {
+                errorLbl.Text = "Error: You must choose a formation";
+                return false;
+            }
+
+            string[] positions = formations.SelectedItem.ToString().Split('-');
+
+            int maxInPos = 0;
+            Position pos = player.PreferredPosition;
+            switch(pos)
+            {
+                case Position.Goalkeeper:
+                    maxInPos = 1;
+                    break;
+                case Position.Defender:
+                    maxInPos = Int32.Parse(positions[0]);
+                    break;
+                case Position.Midfielder:
+                    maxInPos = Int32.Parse(positions[1]);
+                    break;
+                case Position.Forward:
+                    maxInPos = Int32.Parse(positions[2]);
+                    break;
+                default:
+                    return false;
+            }
+
+            int counter = 0;
+            foreach(Player pl in selectedPl)
+            {
+                if(pl.PreferredPosition == pos)
+                {
+                    counter++;
+                }
+            }
+
+            if(counter >= maxInPos)
+            {
+                errorLbl.Text = "Error: Too many people in that position";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Q1/Player.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Position
+{
+    Goalkeeper,
+    Defender,
+    Midfielder,
+    Forward
+}
+namespace Q1
+{
+    public class Player : IComparable<Player>
+    {
+        private string _firstName;
+        private string _surname;
+        private Position _preferredPosition;
+        private DateTime _dateOfBirth;
+
+        private static string[] firstNameSet = { "Sophie", "Grace", "Harry", "Luke", "Michael", "Sean", "Ava", "Jack" };
+        private static string[] lastNameSet = { "O'Neill", "Walsh", "Lynch", "Daly", "Nolan", "McCarthy", "Dunne", "Brennan", "O'Sullivan" };
+        private static int[] monthDaySet = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        private static Random rnd = new Random();
+
+        public string FirstName { get { return _firstName; } }
+        public string Surname { get { return _surname; } }
+        public Position PreferredPosition { get { return _preferredPosition; } }
+        public DateTime DateOfBirth { get { return _dateOfBirth; } }
+        public int Age { get { return AgeCalc(_dateOfBirth); } }
+        
+
+        public Player(Position pos)
+        {
+            _firstName = SelectRnd(firstNameSet, rnd);
+            _surname = SelectRnd(lastNameSet, rnd);
+            _preferredPosition = pos;
+            _dateOfBirth = DateGenerator(rnd);
+        }
+
+        private string SelectRnd(string[] nameSet, Random rnd)
+        {
+            int i = rnd.Next(0, nameSet.Length);
+            return nameSet[i];
+        }
+
+        private DateTime DateGenerator(Random rnd)
+        {
+            DateTime current = DateTime.Now;
+            int year = rnd.Next(current.Year - 30, current.Year - 19);
+            int month = rnd.Next(1, 13);
+            int day;
+            if (month == current.Month)
+            {
+                day = rnd.Next(1, current.Day);
+            } else if (month == 2 && year % 4 == 0)
+            {
+                day = rnd.Next(1, 30);
+            } else
+            {
+                day = rnd.Next(1, monthDaySet[month - 1] + 1);
+            }
+            return new DateTime(year, month, day);
+        }
+
+        private int AgeCalc(DateTime dt)
+        {
+            DateTime current = DateTime.Now;
+            int yearsSince = current.Year - dt.Year;
+            if(dt.Month == current.Month && dt.Day > current.Day)
+            {
+                yearsSince -= 1;
+            }
+            return yearsSince;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {Surname} ({Age}) {PreferredPosition.ToString().ToUpper()}";
+        }
+
+        public int CompareTo(Player other)
+        {
+           if(this.PreferredPosition > other.PreferredPosition)
+           {
+                return 1;
+           } else if(this.PreferredPosition < other.PreferredPosition)
+           {
+                return -1;
+           }
+           else
+           {
+                return this.FirstName.CompareTo(other.FirstName);
+           }
+        }
+    }
+}
